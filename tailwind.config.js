091a96befa316bf249800001ce53a33766d71cd1
Patricio Calvo <patricio@serfe.com>
2026-05-15 15:49:11 -0300

    fix: safelist tailwind gradient classes for dynamic DB colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
+  safelist: [
+    { pattern: /^(from|to|via)-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(100|200|300|400|500|600|700|800|900)$/ },
+  ],
   theme: {
     extend: {
       fontFamily: {
